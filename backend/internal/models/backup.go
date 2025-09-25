a37d680ea63c53862071c4cb0210b5da6ea30090
ryan-ladmia <ryan.ladmia@laplateforme.io>
2025-09-25 17:53:50 +0200

    Ajout du model backup

---
+++ backend/internal/models/backup.go
@@ -0,0 +1,19 @@
+package models
+
+import (
+	"time"
+
+	"gorm.io/gorm"
+)
+
+type Backup struct {
+	Id         uint           `gorm:"primaryKey" json:"id"`
+	Filepath   string         `gorm:"type:text;not null" json:"filepath"`
+	CreatedAt  time.Time      `json:"created_at"`
+	UpdatedAt  time.Time      `json:"updated_at"`
+	DeletedAt  gorm.DeletedAt `gorm:"index" json:"-"`
+	UserId     uint           `gorm:"index;not null" json:"user_id"`
+	User       User           `gorm:"foreignKey:UserId" json:"-"`
+	DatabaseId uint           `gorm:"index;not null" json:"database_id"`
+	Database   Database       `gorm:"foreignKey:DatabaseId" json:"-"`
+}
